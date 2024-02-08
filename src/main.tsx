ce7a47ce59bcb7d5d81fd1de3c7d8ea8ff668747
Eito Takase <eito01178@gmail.com>
2024-02-08 12:05:46 +0900

    500

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { setupMsw } from './msw/browser.ts'
+import { useState, useEffect } from 'react'
 
 setupMsw()
 
+const Delay = () => {
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true)
+    }, 500)
+  }, [])
+  return show ? <App /> : null
+
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <Delay />
 )
